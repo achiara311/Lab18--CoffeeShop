f6c3220ead2d9fc55ceb32c09902086200ad2f10
Anthony Chiara <achiara12@gmail.com>
2019-08-16 14:03:34 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab18CoffeeShop.Models;
+
+namespace Lab18CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View(new RegisterUser()); ;
+        }
+
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View(new RegisterUser());
+        }
+        
+        [HttpPost]
+        public IActionResult Registration(RegisterUser heyThere)
+        {
+            return RedirectToAction("Summary", heyThere);
+        }
+
+        public IActionResult Summary(RegisterUser heyThere)
+        {
+            return View(heyThere);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
