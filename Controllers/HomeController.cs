ca719cc6f3b5002d7cea328c3f09308b14fb0d30
Anthony Chiara <achiara12@gmail.com>
2019-08-20 18:50:45 -0400

    added user list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,21 +5,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Lab18CoffeeShop.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
 
 namespace Lab18CoffeeShop.Controllers
 {
     public class HomeController : Controller
     {
+        List<RegisterUser> registerstuff = new List<RegisterUser>();
+       
+        
         public IActionResult Home()
         {
-            return View();
+            return View(registerstuff);
         }
 
         public IActionResult Index()
         {
             return View(new RegisterUser()); 
         }
-
+        public IActionResult ListOfRegisteredUsers()
+        {
+            string myList = HttpContext.Session.GetString("ListOfPeople");
+            registerstuff = JsonConvert.DeserializeObject<List<RegisterUser>>(myList);
+            return View(registerstuff); 
+           
+        }
+        
         [HttpGet]
         public IActionResult Registration()
         {
@@ -29,8 +41,18 @@ namespace Lab18CoffeeShop.Controllers
         [HttpPost]
         public IActionResult Registration(RegisterUser heyThere)
         {
+           
             if (ModelState.IsValid)
-            {
+            {//this is where you create your sessions
+                string registerLoginJSon = HttpContext.Session.GetString("ListOfPeople");
+                if(registerLoginJSon != null)
+                {
+                    registerstuff = JsonConvert.DeserializeObject<List<RegisterUser>>(registerLoginJSon);
+                }
+                
+                registerstuff.Add(heyThere);
+                HttpContext.Session.SetString("ListOfPeople", JsonConvert.SerializeObject(registerstuff));
+
                 return RedirectToAction("Summary", heyThere); //makes validation work
             }
             else
@@ -47,6 +69,7 @@ namespace Lab18CoffeeShop.Controllers
             
         }
 
+       /* 
         [HttpPost] //HttpGet is created by default?
         public IActionResult DisplayInfo(RegisterUser user)
         {
@@ -58,9 +81,27 @@ namespace Lab18CoffeeShop.Controllers
             {
                 return View("Index", user); //goes to Error in the HomeController
             }
+        */
+
+        public IActionResult LoginForm()
+        {
+            return View();
         }
-
-
+        [HttpPost]
+        public IActionResult SaveLoginInfo(RegisterUser guy, string SaveLogin)
+        {
+            if (SaveLogin == "yes")
+            {
+                //add a cookie to the browser to save the user's payment information
+                CookieOptions option = new CookieOptions();
+                Response.Cookies.Append("UserLoginName", guy.UserName, option); //cant store entire object in cookie
+                //include properties that you want to keep
+                Response.Cookies.Append("UserLoginPassword", guy.Password, option);
+                //Response.Cookies.Append("CreditCardNumber", credit.Number, option);
+            }
+           // return RedirectToAction("Index", guy);
+            return RedirectToAction("Home", guy);
+        }
         public IActionResult Privacy()
         {
             return View();
